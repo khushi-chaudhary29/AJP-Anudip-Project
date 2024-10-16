d16f7e2d2088e83fde8a724edfe7342edb306b46
Khushi Chaudhary <khushichdry2912@gmail.com>
2024-10-16 11:27:22 +0530

    some change

--- Lab4b.java
+++ Lab4b.java
@@ -1,10 +1,14 @@
 public class Lab4b{
     public static void main(String[] args){
-        int[] arr={1,2,3,4,5};
-
+	
+    int[] arr = new int[100];
+    for (int i = 0; i < arr.length; i++) {
+             arr[i] = i + 1; 
+    }
+        System.out.println("Elements from 1 to 100 in array are: ");
         try{
             for(int i = 0 ; i<=arr.length; i++){
-                System.out.println("Elements at index "+i+" : "+arr[i]);
+                System.out.print(arr[i]+" ");
             }
         }catch(ArrayIndexOutOfBoundsException e){
             System.out.println("Error: Array index out of bounds.");
